713f3742e61f9a4e6be7cbe23047d8629b3259a8
potinglee <potinglee999@gmail.com>
2017-06-07 11:22:25 +0800

    remove I2C useless part

--- 9_I2C/lcd.c
+++ 9_I2C/lcd.c
@@ -39,15 +39,6 @@ void gpio_init() {
   GPIO_Init(GPIOC, &GPIO_InitStructure);
 
   GPIO_PinAFConfig(GPIOC, GPIO_PinSource9, GPIO_AF_I2C3);
-
-
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_Init(GPIOD, &GPIO_InitStructure);
 }
 
 void i2c_init() {
